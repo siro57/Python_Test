bcbf5b3fa0e11c4993bc19af73a8fdfc6354c4d5
linenworld <linenworld.3603@gmail.com>
2025-07-24 11:47:33 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+def greet():
+    print("こんにちは")
+greet()
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+print_name("しろ")
+
+def get_greet():
+    print("おはようございます")
+get_greet()
+
+def add(a,b):
+    return a + b
+print(add(1,2))
+
